856ac770bf750b2e70420aedf8db24e3b26cbde0
aspire87 <aspire_87@list.ru>
2018-04-12 14:59:01 +0300

    Class Point

---
+++ Snake/Snake/Point.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char sym;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition( x, y );
+            Console.Write(sym);
+        }
+    }
+}
